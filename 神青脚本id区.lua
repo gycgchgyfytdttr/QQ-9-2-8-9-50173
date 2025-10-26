612eaa8749de27e20735afcfb2f368f1a63bfc5e
gycgchgyfytdttr <18058643061@163.com>
2025-10-26 12:33:21 +0800

    Add files via upload

--- 神青脚本id区.lua
+++ 神青脚本id区.lua
@@ -1,4 +1,45 @@
 return {
-    "device_id_1",
-    "2bd4d2ad-ca9e-4a79-be53-09eb75a794d4"
+
+    ["2bd4d2ad-ca9e-4a79-be53-09eb75a794d4"] = "KEY_f4664976773sjjdjdie",
+    
+    ["d1bf30ec-d07f-45e7-9153-5c286009671a"] = "KEY_jdjdndjxijxdkskzmjdjid464679",
+    
+        ["ce261784-3ef3-4893-b224-92577f1c2592"] = "KEY_V46466djjdkzjdk1346qaz",
+    
+    ["f159e0c7-77a2-4e4b-b9d6-642d782f56f5"] = "KEY_f4664976773sjjdjdie",
+    
+    ["cc3b9731-087d-44cf-a350-4a723fc3caec"] = "V3X4KFG4369V845UIE2",
+    
+    ["19fb2d22-8c72-4816-992b-d7612d2eaecd"] = "KEY_a8d3e7f2g5h9j1k4m6n",
+    
+    ["25c05aae-ff09-43f2-8cbb-69cdf4b97af4"] = "V3YE6464iFdEWW334",
+    
+    ["91f2f674-e9e9-4994-82dc-1920ed7f2145"] = "KEY_b9c4d6e8f1g3h5j7k2m",
+    
+    ["880048b0-afd7-4b57-be15-a8d1d4c620f5"] = "KEY_e6h9k2m5n8p3r6t9v2",
+    
+        ["0ef778d6-4ad0-45bc-85d7-110e019694f7"] = "KEY_jsdiwiwozkmxnfhtyruieiskd",
+    
+    ["0bac085d-8cd5-469b-b528-9eedf725583e"] = "KEY_V32X4v1bhj93",
+    
+    ["069d48f2-599e-4055-8521-3f3ce42bb328"] = "KEY_x9a6d2g5j8m3p6s9v3",
+ 
+ ["b2f03b43-8c52-4271-a8ba-4739cdacba47"] = "KEY_V3js4673sksjixz",
+ 
+ ["ee78ea53-1808-4138-8e4f-d4add7e2e469"] = "KEY_v8z5c9f3i6l9o3r8u2",
+ 
+ ["e90a9e3e-af2a-430c-87d5-e501d36b2345"] = "KEY_s3z6c9f4j7m1p4s7v1",
+ 
+     ["af2795b1-b9c6-44c1-953b-1f9d06a2dc7d"] = "KEY_11451454188",
+ 
+    ["8b53841f-0c18-43c4-bf08-4ddb28370ea8"] = "KEY_g6n2q5t8w3a6d9g2j4",
+    
+    ["e899953c-42ab-43d8-9d00-77cc0eb4155f"] = "KEY_ghg575nnhghy242",
+    
+        ["7ad0415d-b3f6-4751-88c2-0cb43f8b5fe3"] = "KEY_69usjsjsjkzk4646",
+        
+    ["45987840-26e2-4ca4-86b2-5c63d4add1a8"] = "KEY_j8q5t8w3a6d9g2j4m6",
+         
+    ["4e8f9d17-948c-4ff7-8482-34798b661dd2"] = "KEY_c2f4g6h8j1k3m5n7p9"
+    
 }
