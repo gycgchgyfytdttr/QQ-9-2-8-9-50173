3f1b4e633a4c918ab8750b2e83536de69f093336
gycgchgyfytdttr <18058643061@163.com>
2025-11-05 22:27:16 +0800

    Add files via upload

--- 旧版本whitelist.lua
+++ 旧版本whitelist.lua
@@ -5,5 +5,6 @@ return {
     "c95e6158-0994-4d3c-866b-c38cc16f85b6",
     "65398900-8b14-4987-bc7b-55c66d332575",
     "cdbe1f5c-b85c-4fb3-83fb-cf5ea847e50c",
+    "b8eca845-4cf0-4213-805c-63d4c303c413",
     "e83b54d4-a94e-4a5f-9ae6-0aa4bc6ad16a"
 }
