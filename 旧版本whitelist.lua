fb95a0a3f23b9aee119e4a054c44ff23d9f3fa9b
gycgchgyfytdttr <18058643061@163.com>
2025-11-05 12:13:57 +0800

    Add files via upload

--- 旧版本whitelist.lua
+++ 旧版本whitelist.lua
@@ -3,5 +3,6 @@ return {
     "6f1af27a-7c6c-42f6-a48f-d29b3f1c4188", 
     "2bd4d2ad-ca9e-4a79-be53-09eb75a794d4",
     "c95e6158-0994-4d3c-866b-c38cc16f85b6",
+    "65398900-8b14-4987-bc7b-55c66d332575",
     "e83b54d4-a94e-4a5f-9ae6-0aa4bc6ad16a"
 }
